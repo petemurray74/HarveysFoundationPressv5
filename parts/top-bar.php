d1fb0f51c4890fd7e919d0cf02b2ad08c766b03e
PeteM <github@thrap.co.uk>
2015-03-01 22:42:04 +0000

    remove branding from menu

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -1,10 +1,5 @@
 <div class="top-bar-container contain-to-grid show-for-medium-up">
     <nav class="top-bar" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <h4><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h4>
-            </li>
-        </ul>
         <section class="top-bar-section">
             <?php foundationPress_top_bar_l(); ?>
             <?php foundationPress_top_bar_r(); ?>
