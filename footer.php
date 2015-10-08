67f6bb2e092509b199574818269c34e5f3548ddf
petemurray74 <github@thrap.co.uk>
2015-10-08 21:38:39 +0100

    new footer

--- footer.php
+++ footer.php
@@ -12,7 +12,6 @@
 ?>
 
 </section>
-<<<<<<< HEAD
 <aside class="row">
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
@@ -24,16 +23,6 @@
     get_template_part('parts/footer-area');
     do_action('foundationPress_layout_end');
     ?>
-=======
-<footer class="row">
-	<?php do_action( 'foundationpress_before_footer' ); ?>
-	<?php dynamic_sidebar( 'footer-widgets' ); ?>
-	<?php do_action( 'foundationpress_after_footer' ); ?>
-</footer>
-<a class="exit-off-canvas"></a>
-
-	<?php do_action( 'foundationpress_layout_end' ); ?>
->>>>>>> 115f1b31ec14cad002fa0f8dad739d3bcf34e18f
 	</div>
 </div>
 <?php wp_footer(); ?>
